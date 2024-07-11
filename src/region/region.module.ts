94ec3dd5d1c73017404fc149b206404711404fe1
ja1423 <aziabdujabborova1423@gmail.com>
2024-07-11 12:37:26 +0500

    region

--- src/region/region.module.ts
+++ src/region/region.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { RegionController } from './region.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Region } from './model/region.model';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Region]), JwtModule.register({})],
+
   controllers: [RegionController],
   providers: [RegionService],
 })
