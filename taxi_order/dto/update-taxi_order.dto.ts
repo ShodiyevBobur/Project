67509a4abaa92a825b5c85e8ee505fff9c210b13
ja1423 <aziabdujabborova1423@gmail.com>
2024-07-11 12:16:35 +0500

    .

--- taxi_order/dto/update-taxi_order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTaxiOrderDto } from './create-taxi_order.dto';
-
-export class UpdateTaxiOrderDto extends PartialType(CreateTaxiOrderDto) {}
